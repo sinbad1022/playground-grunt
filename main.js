0f9b48c35ec149f7c7b9268882986bc5ce9c0b58
sinbad1022 <sinbad1022@gmail.com>
2014-06-09 13:58:58 +0800

    playground

---
+++ main.js
@@ -0,0 +1,24 @@
+require.config({
+    baseUrl:"",
+    shim : {
+        $: {
+            exports: 'zepto'
+        },
+        _: {
+            exports: '_'
+        },
+        B: {
+            deps: ['_','$'],
+            exports: 'Backbone'
+        }
+    },
+    paths: {
+        '$': 'src/zepto',
+        '_': 'src/underscore',
+        'B': 'src/backbone'
+    }
+});
+
+requirejs(['B'], function (b) {
+            alert("uuuuuu");
+});
---
+++ src/backbone.js
@@ -0,0 +1,1 @@
+var backbone = "backbone";
